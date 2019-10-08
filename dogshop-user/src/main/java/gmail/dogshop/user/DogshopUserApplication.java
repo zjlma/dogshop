747bea19c7984e76e77462ca6434aeb0d8c99f85
zenjinlin <2559810994@qq.com>
2019-10-08 12:34:30 +0800

    19/10/8

---
+++ dogshop-user/src/main/java/gmail/dogshop/user/DogshopUserApplication.java
@@ -0,0 +1,13 @@
+package gmail.dogshop.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogshopUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogshopUserApplication.class, args);
+    }
+
+}
